55268ddb195139c2b93434cc7c2a275182d199a9
sangheon <qwer55252@gmail.com>
2025-05-20 11:26:15 +0000

    Add: 실험 스크립트 추가

--- scripts/train_tedlium_magbased.sh
+++ scripts/train_tedlium_magbased.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=2 python train_IRHP_tedlium.py \
---per_device_train_batch_size 4 \
---output_dir outputs/tedlium \
+--per_device_train_batch_size 12 \
+--output_dir outputs/tedlium_magbased \
 --method "magnitude-based" \
 --dataset "tedlium"
--- scripts/train_tedlium_oneshot.sh
+++ scripts/train_tedlium_oneshot.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=2 python train_IRHP_tedlium.py \
 --per_device_train_batch_size 12 \
---output_dir outputs/tedlium_IRHP \
---method "redundancy-based" \
+--output_dir outputs/tedlium_oneshot \
+--method "one-shot" \
 --dataset "tedlium"
