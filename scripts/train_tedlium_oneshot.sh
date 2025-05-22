963a27d2ef56e16897f4131fc477529a8558698c
sangheon <qwer55252@gmail.com>
2025-05-22 07:32:48 +0000

    Add: one-shot 학습 코드 추가

--- scripts/train_tedlium_oneshot.sh
+++ scripts/train_tedlium_oneshot.sh
@@ -1,5 +1,4 @@
-CUDA_VISIBLE_DEVICES=2 python train_IRHP_tedlium.py \
+python train_oneshot_tedlium.py \
 --per_device_train_batch_size 12 \
 --output_dir outputs/tedlium_oneshot \
---method "one-shot" \
 --dataset "tedlium"
