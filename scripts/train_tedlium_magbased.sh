20801663b9b1b98c38ad4df89010d5da84fda3c3
sangheon <qwer55252@gmail.com>
2025-05-22 09:07:06 +0000

    Fix: mag-based 스크립트 수정

--- scripts/train_tedlium_magbased.sh
+++ scripts/train_tedlium_magbased.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=2 python train_IRHP_tedlium.py \
---per_device_train_batch_size 12 \
+python train_IRHP_tedlium.py \
+--per_device_train_batch_size 8 \
 --output_dir outputs/tedlium_magbased \
 --method "magnitude-based" \
 --dataset "tedlium"
