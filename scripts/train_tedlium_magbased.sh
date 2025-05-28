3f095aad40680bda44295d462dddf384093a485b
sangheon <qwer55252@gmail.com>
2025-05-28 08:56:53 +0000

    Fix: tedlium magbased scripts 수정

--- scripts/train_tedlium_magbased.sh
+++ scripts/train_tedlium_magbased.sh
@@ -1,5 +1,7 @@
+EXP_NAME=tedlium_magbased
+
 python train_IRHP_tedlium.py \
 --per_device_train_batch_size 8 \
---output_dir outputs/tedlium_magbased \
+--output_dir outputs/${EXP_NAME} \
 --method "magnitude-based" \
 --dataset "tedlium"
