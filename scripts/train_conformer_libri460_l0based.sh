ac3d3f206b14d441e98d4fe5eaac955476319aa8
sangheon <qwer55252@gmail.com>
2025-06-10 05:33:57 +0000

    Add: train scripts

--- scripts/train_conformer_libri460_l0based.sh
+++ scripts/train_conformer_libri460_l0based.sh
@@ -1,19 +1,21 @@
 export HF_DATASETS_CACHE="/root/.cache/huggingface/datasets"
-export PRJ_NAME="conformer"
-export EXP_NAME="conformer_libri100_baseline"
+export PRJ_NAME="conformer_libri460"
+export EXP_NAME="conformer_libri460_l0based_testmode"
 
 # 1) 출력 디렉토리 생성
 OUTPUT_DIR="./outputs/$EXP_NAME"
 mkdir -p "$OUTPUT_DIR"
 
 # 2) 학습 실행 및 로그 저장
-CUDA_VISIBLE_DEVICES=2 python train_conformer_IRHP_libri.py \
+CUDA_VISIBLE_DEVICES=1 python train_conformer_IRHP_libri.py \
 --output_dir "$OUTPUT_DIR" \
---data_config_name train_100 \
---data_train_split train.clean.100 \
+--data_config_name train_460 \
+--data_train_split train.clean.100+train.clean.360 \
 --data_val_split dev.clean \
 --data_test_split test.clean \
 --batch_size 32 \
 --final_finetune_epochs 100 \
---method "baseline" \
+--method "l0-based" \
+--prune_ratio 0.5 \
 --dataset_name librispeech \
+--test_mode
