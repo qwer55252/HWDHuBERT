724cff5ab5f87bea57f5a653afbbd7af7614dc7d
sangheon <qwer55252@gmail.com>
2025-06-10 05:10:51 +0000

    Add: train scripts

--- scripts/train_conformer_libri460_IRHP.sh
+++ scripts/train_conformer_libri460_IRHP.sh
@@ -1,6 +1,6 @@
 export HF_DATASETS_CACHE="/root/.cache/huggingface/datasets"
-export PRJ_NAME="conformerlarge"
-export EXP_NAME="conformerlarge_libri100_IRHP"
+export PRJ_NAME="conformer_libri460"
+export EXP_NAME="conformer_libri460_IRHP_testmode"
 
 # 1) 출력 디렉토리 생성
 OUTPUT_DIR="./outputs/$EXP_NAME"
@@ -9,14 +9,13 @@ mkdir -p "$OUTPUT_DIR"
 # 2) 학습 실행 및 로그 저장
 python train_conformer_IRHP_libri.py \
 --output_dir "$OUTPUT_DIR" \
---data_config_name train_100 \
---data_train_split train.clean.100 \
+--data_config_name train_460 \
+--data_train_split train.clean.100+train.clean.360 \
 --data_val_split dev.clean \
 --data_test_split test.clean \
---batch_size 8 \
---iterative_finetune_epochs 2 \
+--batch_size 32 \
 --final_finetune_epochs 100 \
 --method "redundancy-based" \
---prune_ratio 0.8 \
+--prune_ratio 0.5 \
 --dataset_name librispeech \
---model_name_or_path stt_en_conformer_ctc_large \
+--test_mode
